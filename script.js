6984ca8698ccd6ae36331a7fc89ed78da04d460d
eidriB <sara.holmquist.syne21lin@tucsweden.se>
2021-12-02 23:49:36 +0100

    Funktion som kallar på info till date från apl

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //API KEY:
 //https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69
 
-function getXML() {
+let date = document.getElementById("prev-date");
+
+function getAPOD() {
     let xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69');
@@ -15,5 +17,28 @@ function getXML() {
     xhr.send();
 }
 
+date.onchange = function(){
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.nasa.gov/planetary/apod?date=' + date.value + '&api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69');
+
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        var c = document.getElementById("prev-apod");
+
+        console.log(xhr.response);
+        if (xhr.response.code == 400) {
+            c.getElementsByTagName("h3")[0].innerText = "Not a valid date"
+        }else{
+            c.getElementsByTagName("img")[0].src = xhr.response.url;
+            c.getElementsByTagName("p")[0].innerText = xhr.response.explanation;
+            c.getElementsByTagName("h3")[0].innerText = xhr.response.title;
+        }
+    };
+
+    xhr.send();
+}
+
 //Körs när js laddas
-getXML();
+getAPOD();
