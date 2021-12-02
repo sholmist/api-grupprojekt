3cf5995d76ca1aef370f60f26bee928a07520f8d
Royne Reinholdsson <royne@outlook.com>
2021-12-02 17:30:25 +0100

    region vid header i JS

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 //API KEY:
 //https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69
 
+//#region Header
+function header() {
 
-function header(){
+    let xhr = new XMLHttpRequest();
 
-let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69");
 
-xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69");
+    xhr.responseType = "json";
 
-xhr.responseType = "json";
+    xhr.onload = function () {
 
-xhr.onload = function () {
 
-    
-    document.querySelector(".headerbg").style.backgroundImage = "url('" + xhr.response.url + "')";
-    console.log(xhr.response.url);
-        
-};
+        document.querySelector(".headerbg").style.backgroundImage = "url('" + xhr.response.url + "')";
+        console.log(xhr.response.url);
 
-xhr.send();
+    };
+
+    xhr.send();
 
 };
 
 header();
+
+//#endregion
+
 function getXML() {
     let xhr = new XMLHttpRequest();
 
