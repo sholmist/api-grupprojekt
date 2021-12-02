7a41dceb4063a0e49fc127919bca5679b3223d19
Tine Steén <tine.steen.syne21lin@tucsweden.se>
2021-12-03 00:00:46 +0100

    Städar onödiga radbrytningar

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-//API KEY:
-//https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69
-
 let api_key = 'CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69';
 let year;
 let month;
 let day;
 
-
 //#region Header
 function header() {
-
     let xhr = new XMLHttpRequest();
-
     xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=CNCFz3LTIegsRtNzARWJShPRpuzRXlCjtC0p1K69");
-
     xhr.responseType = "json";
-
     xhr.onload = function () {
-
-
-        document.querySelector(".headerbg").style.backgroundImage = "url('" + xhr.response.url + "')";
-        console.log(xhr.response.url);
-
+document.querySelector(".headerbg").style.backgroundImage = "url('" + xhr.response.url + "')";
+    console.log(xhr.response.url);
     };
-
     xhr.send();
-
 };
-
 header();
-
 //#endregion
 
-
-
-
-
-
-
-
-
-
-
 // Funktioner för att hämta och visa marsrover
-
 function showImage(url) {
-
     document.getElementById("start_image").src = url;
-
     return `visar ${url}`;
-
 }
-
-
-
 
 async function planetPicker(planet) {
     switch (planet) {
@@ -83,20 +51,12 @@ async function planetPicker(planet) {
             break;
     }
     year = '2015';
-
     month = '06';
-
     day = '03';
-
     let result = await fetch(url)
-
         .then(response => response.json())
-
         .then(data => {
-
             showImage(data.photos[0].img_src)
-
         });
-
     console.log(result);
 }
